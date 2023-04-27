455878438f506bfba1d60982cbe765ed83d7efd1
promokk <43420577+promokk@users.noreply.github.com>
2023-04-27 23:31:15 +0300

    Add files via upload

---
+++ lab6/blog/articles/views.py
@@ -0,0 +1,68 @@
+from django.urls import reverse
+from .models import Article
+from django.shortcuts import redirect, render
+from django.http import Http404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+
+def archive(request):
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
+
+def get_article(request, article_id):
+    try:
+        post = Article.objects.get(id=article_id)
+        return render(request, 'article.html', {"post": post})
+    except Article.DoesNotExist:
+        raise Http404
+    
+def create_post(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+        # обработать данные формы, если метод POST
+            form = {
+                'text': request.POST["text"], 'title': request.POST["title"]
+            }
+        # в словаре form будет храниться информация, введенная пользователем
+            if form["text"] and form["title"]:
+        # если поля заполнены без ошибок
+                if Article.objects.filter(title=form["title"]).exists():
+                    form['errors'] = u"Запись с таким названием существует!"
+                    return render(request, 'create_post.html', {'form': form})
+                article = Article.objects.create(text=form["text"], title=form["title"], author=request.user)
+                return redirect('get_article', article_id=article.id)
+            # перейти на страницу поста
+            else:
+        # если введенные данные некорректны
+                form['errors'] = u"Не все поля заполнены"
+                return render(request, 'create_post.html', {'form': form})
+        else:
+        # просто вернуть страницу с формой, если метод GET
+            return render(request, 'create_post.html', {})
+
+    else:
+        raise Http404
+    
+def delete_post(request, article_id):
+    if Article.objects.filter(pk=article_id).exists():
+            Article.objects.filter(pk=article_id).delete()
+            return redirect(reverse('archive'))
+    else:
+        raise Http404
+
+def registration(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # получаем имя пользователя и пароль из формы
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            # выполняем аутентификацию
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
